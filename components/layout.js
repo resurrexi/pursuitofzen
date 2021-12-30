80598074ae5cf710e7d65ad3fe15725b03703449
Liquan Yang <thedevhiro@gmail.com>
2021-12-30 21:21:13 +0800

    Add bio to home

---
+++ components/layout.js
@@ -0,0 +1,3 @@
+export default function Layout({ children }) {
+  return <div className="relative bg-white max-w-7xl mx-auto px-4 py-16 sm:px-6 lg:px-8">{children}</div>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: colors.cyan,
+      secondary: colors.violet,
+      gray: colors.slate,
+      white: colors.white,
+    },
     extend: {},
   },
   plugins: [],
