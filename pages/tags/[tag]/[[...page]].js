a3c4aa07bff2606063fa6fe9477a9d0847942777
Liquan Yang <thedevhiro@gmail.com>
2022-01-05 23:06:23 +0800

    fix resolution of optional page param

---
+++ pages/tags/[tag]/[[...page]].js
@@ -0,0 +1,94 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Layout from "../../../components/layout";
+import PostList from "../../../components/post-list";
+import { getAllPosts } from "../../../lib/posts";
+
+const PAGE_SIZE = 10;
+
+export async function getStaticPaths() {
+  const tagPosts = await getAllPosts(true);
+  const tagPages = tagPosts.map((tagPost) => {
+    // determine pages required for tag
+    const pages = Math.ceil(tagPost.total / PAGE_SIZE);
+
+    // build page number array
+    const pageIterator = Array.from(Array(pages).keys()).map((i) => i + 1);
+
+    // generate paths for the tag/page combinations
+    const tagPagePaths = pageIterator.map((i) => {
+      return {
+        params: {
+          tag: tagPost.tag,
+          page: [i.toString()],
+        },
+      };
+    });
+
+    // generate paths for tag only
+    const tagPath = {
+      params: {
+        tag: tagPost.tag,
+        page: [],
+      },
+    };
+
+    return [...tagPagePaths].concat([tagPath]);
+  });
+
+  return {
+    paths: tagPages.flat(),
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  let resolvedPage = 1;
+  const allPosts = await getAllPosts(true);
+  const tagData = allPosts.find((tagPost) => tagPost.tag === params.tag);
+
+  if (params.page && params.page.length > 0) {
+    resolvedPage = params.page[0];
+  }
+
+  const subsetPosts = tagData.posts.slice(
+    (resolvedPage - 1) * PAGE_SIZE,
+    resolvedPage * PAGE_SIZE,
+  );
+
+  return {
+    props: {
+      tag: params.tag,
+      posts: subsetPosts,
+      // return 0 for currentPage if page argument is invalid
+      currentPage: subsetPosts.length === 0 ? 0 : parseInt(resolvedPage),
+      totalPages: Math.ceil(tagData.total / PAGE_SIZE),
+    },
+    revalidate: 10,
+  };
+}
+
+export default function TagPage({ tag, posts, currentPage, totalPages }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // if currentPage is 0, then page param is invalid
+    if (currentPage === 0) {
+      router.replace("/tags/[tag]", `/tags/${tag}`);
+    }
+  }, [currentPage, tag, router]);
+
+  return (
+    <Layout pageTitle={`Browse by ${tag}`}>
+      <main className="min-w-[80vw] sm:min-w-[85vw] md:min-w-[42rem]">
+        {posts && posts.length > 0 && (
+          <PostList>
+            {posts.map(({ slug, meta }) => (
+              <PostList.Item key={slug} slug={slug} meta={meta} />
+            ))}
+          </PostList>
+        )}
+      </main>
+    </Layout>
+  );
+}
