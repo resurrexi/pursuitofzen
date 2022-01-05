5328342b1aa482794f26d5dc492633501d16895c
Liquan Yang <thedevhiro@gmail.com>
2022-01-05 23:43:08 +0800

    add loading text

--- components/loading-container.js
+++ components/loading-container.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function LoadingContainer() {
   return (
     <div className="container mx-auto">
-      <div className="flex place-content-center min-h-screen">
+      <div className="flex flex-col place-content-center min-h-screen">
         <Image
           src="/logo.svg"
           alt="loading..."
@@ -11,6 +11,11 @@ export default function LoadingContainer() {
           height={45}
           className="animate-[revspin_1s_ease-in-out_infinite]"
         />
+        <div className="pt-6 text-center">
+          <h1 className="animate-pulse text-lg font-bold text-gray-600 dark:text-gray-400">
+            Loading...
+          </h1>
+        </div>
       </div>
     </div>
   );
