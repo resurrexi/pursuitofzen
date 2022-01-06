915b7668b1c17a6914deb87d4b893a8d0b153be9
Liquan Yang <thedevhiro@gmail.com>
2022-01-06 14:55:49 +0800

    add hamburger menu

--- components/header.js
+++ components/header.js
@@ -1,17 +1,34 @@
+import { useState, createContext, useContext, useCallback } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import DarkModeToggler from "./dark-mode-toggler";
+import HamburgerMenuIcon from "./icons/hamburger";
+import CloseIcon from "./icons/close";
+
+export const NavContext = createContext();
+
+export function useMenuToggle() {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const toggleMenu = useCallback(() => {
+    setMenuOpened((prev) => !prev);
+  }, [setMenuOpened]);
+
+  return { menuOpened, toggleMenu };
+}
 
 function Header({ children }) {
   return <header className="flex-none">{children}</header>;
 }
 
 function NavBar({ children }) {
+  // for hamburger menu
+  const { menuOpened, toggleMenu } = useContext(NavContext);
+
   return (
     <nav>
-      <div className="max-w-3xl mx-auto px-4 border-b border-gray-200 sm:px-6 lg:px-8 dark:border-gray-600">
-        <div className="flex justify-between h-16">
-          <div className="flex">
+      <div className="relative max-w-3xl mx-auto px-4 border-b border-gray-200 sm:px-6 lg:px-8 dark:border-gray-600">
+        <div className="flex static justify-between h-16">
+          <div className="flex static">
             <div className="flex-shrink-0 flex items-center sm:mr-6">
               <Link href="/">
                 <a>
@@ -21,8 +38,20 @@ function NavBar({ children }) {
             </div>
             {children}
           </div>
-          <div className="hidden sm:flex">
+          <div className="flex items-center hidden sm:flex">
             <DarkModeToggler />
+          </div>
+          <div className="flex items-center sm:hidden">
+            <span className="sr-only">Open main menu</span>
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-500 hover:text-gray-600 hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:text-gray-300 dark:hover:bg-gray-800"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={toggleMenu}
+            >
+              {menuOpened ? <CloseIcon /> : <HamburgerMenuIcon />}
+            </button>
           </div>
         </div>
       </div>
@@ -31,13 +60,28 @@ function NavBar({ children }) {
 }
 
 function NavMenu({ children }) {
-  return <div className="hidden sm:flex sm:space-x-4">{children}</div>;
+  const { menuOpened } = useContext(NavContext);
+
+  return (
+    <>
+      <div className="hidden sm:flex sm:space-x-4">{children}</div>
+      {/* mobile menu */}
+      {menuOpened && (
+        <div
+          className="absolute left-0 top-full px-4 bg-white w-screen h-screen border-t border-gray-200 sm:hidden dark:border-gray-600"
+          id="mobile-menu"
+        >
+          <div className="pt-2 pb-3 space-y-1">{children}</div>
+        </div>
+      )}
+    </>
+  );
 }
 
 function NavMenuItem({ link, label }) {
   return (
     <Link href={link}>
-      <a className="inline-flex text-gray-500 items-center text-base font-bold hover:text-primary-500 dark:text-gray-400 dark:hover:text-primary-600">
+      <a className="block py-2 text-gray-500 items-center font-bold text-lg sm:text-base sm:inline-flex hover:text-primary-500 dark:text-gray-400 dark:hover:text-primary-600">
         {label}
       </a>
     </Link>
--- components/layout.js
+++ components/layout.js
@@ -6,11 +6,18 @@ import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-python";
 import "prismjs/plugins/line-numbers/prism-line-numbers";
 import Header from "./header";
+import { NavContext, useMenuToggle } from "./header";
 import LoadingContainer from "./loading-container";
 
 export default function Layout({ children, pageTitle }) {
   const [loaded, setLoaded] = useState(false);
   const router = useRouter();
+  const { menuOpened, toggleMenu } = useMenuToggle();
+
+  const menuContext = {
+    menuOpened,
+    toggleMenu,
+  };
 
   useEffect(() => {
     if (!router.isFallback) {
@@ -25,52 +32,61 @@ export default function Layout({ children, pageTitle }) {
   }, [loaded]);
 
   return (
-    <div className="flex flex-col min-h-screen bg-white transition-colors duration-1000 ease-in-out dark:bg-neutral-900">
-      <Head>
-        <title>Pursuit of Zen - {pageTitle}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="description" content="Personal website of Liquan Yang" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <Header>
-        <Header.Nav>
-          <Header.NavMenu>
-            <Header.NavMenuItem link="/blog" label="Blog" />
-            <Header.NavMenuItem link="/works" label="Works" />
-            <Header.NavMenuItem link="/resume" label="Resume" />
-          </Header.NavMenu>
-        </Header.Nav>
-      </Header>
-      {loaded ? (
-        <LoadingContainer />
-      ) : (
-        <div className="flex-auto max-w-3xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
-          {children}
-        </div>
-      )}
-      <footer className="flex-none">
-        <div className="border-t border-gray-200 text-gray-500 max-w-3xl mx-auto px-4 py-4 sm:px-6 lg:px-8 dark:text-gray-400 dark:border-gray-600">
-          &copy; {new Date().getFullYear()} Pursuit of Zen
-        </div>
-      </footer>
-    </div>
+    <NavContext.Provider value={menuContext}>
+      <div
+        className={`${
+          menuOpened && "max-h-screen"
+        } flex flex-col overflow-hidden min-h-screen bg-white transition-colors duration-1000 ease-in-out dark:bg-neutral-900 sm:overflow-auto sm:max-h-full`}
+      >
+        <Head>
+          <title>Pursuit of Zen - {pageTitle}</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta name="description" content="Personal website of Liquan Yang" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
+        <Header>
+          <Header.Nav>
+            <Header.NavMenu>
+              <Header.NavMenuItem link="/blog" label="Blog" />
+              <Header.NavMenuItem link="/works" label="Works" />
+              <Header.NavMenuItem link="/resume" label="Resume" />
+            </Header.NavMenu>
+          </Header.Nav>
+        </Header>
+        {!loaded ? (
+          <LoadingContainer />
+        ) : (
+          <div className="flex-auto max-w-3xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        )}
+        <footer className="flex-none">
+          <div className="border-t border-gray-200 text-gray-500 max-w-3xl mx-auto px-4 py-4 sm:px-6 lg:px-8 dark:text-gray-400 dark:border-gray-600">
+            &copy; {new Date().getFullYear()} Pursuit of Zen
+          </div>
+        </footer>
+      </div>
+    </NavContext.Provider>
   );
 }
