0e1b4ef5fae6251a1bf54566807ffca6da0e0373
Liquan Yang <thedevhiro@gmail.com>
2022-01-06 11:19:53 +0800

    improved loading container

--- components/loading-container.js
+++ components/loading-container.js
@@ -1,21 +1,19 @@
 import Image from "next/image";
 
-export default function LoadingContainer() {
+export default function LoadingContainer(props) {
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col place-content-center min-h-screen">
-        <Image
-          src="/logo.svg"
-          alt="loading..."
-          width={45}
-          height={45}
-          className="animate-[revspin_1s_ease-in-out_infinite]"
-        />
-        <div className="pt-6 text-center">
-          <h1 className="animate-pulse text-lg font-bold text-gray-600 dark:text-gray-400">
-            Loading...
-          </h1>
-        </div>
+    <div className="flex flex-auto flex-col place-content-center" {...props}>
+      <Image
+        src="/logo.svg"
+        alt="loading..."
+        width={30}
+        height={30}
+        className="animate-[revspin_1s_ease-in-out_infinite]"
+      />
+      <div className="mt-4 text-center">
+        <h1 className="animate-pulse text-base font-normal text-gray-600 dark:text-gray-400">
+          Loading...
+        </h1>
       </div>
     </div>
   );
