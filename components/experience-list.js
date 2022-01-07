bcc39223a5a02ca325ff31c3b0ba086023cda4d5
Liquan Yang <thedevhiro@gmail.com>
2022-01-08 00:02:47 +0800

    fix rendering of experience lists

--- components/experience-list.js
+++ components/experience-list.js
@@ -1,4 +1,5 @@
-import CheckCircleIcon from "./icons/check-circle";
+import StarIcon from "./icons/star";
+import CheckIcon from "./icons/check";
 
 function ExperienceList({ children }) {
   return (
@@ -19,20 +20,14 @@ function ExperienceItem({
   isLast,
 }) {
   return (
-    <li className={`${!isLast ? "pb-4" : ""} relative`}>
-      {/* !isLast && (
-        <div
-          className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-secondary-600 dark:bg-secondary-500"
-          aria-hidden="true"
-        />
-      ) */}
+    <li className={`${!isLast ? "pb-6" : ""} relative`}>
       <div className="relative flex items-start">
         <span className="h-9 flex items-center">
-          <span className="relative z-[5] w-8 h-8 flex items-center justify-center">
-            <CheckCircleIcon className="absolute h-5 w-5 text-secondary-600 transition-colors duration-1000 ease-in-out bg-white dark:bg-neutral-900 dark:text-secondary-500" />
+          <span className="hidden relative z-10 w-5 h-5 sm:flex sm:items-center sm:justify-center">
+            <StarIcon className="absolute h-full w-full text-secondary-600 dark:text-secondary-500" />
           </span>
         </span>
-        <span className="ml-4 min-w-0 flex flex-col">
+        <span className="min-w-0 flex flex-col sm:ml-4">
           <span className="flex flex-col sm:inline-block sm:space-x-2 sm:divide-x-2 sm:divide-gray-200 dark:divide-gray-800">
             <span className="text-sm font-semibold uppercase text-gray-900 sm:text-base dark:text-gray-100">
               {primary}
@@ -52,11 +47,14 @@ function ExperienceItem({
             {dates}
           </span>
           {descriptionList && descriptionList.length > 0 && (
-            <span className="text-sm text-gray-500 sm:text-base dark:text-gray-400">
-              <ul className="list-disc list-inside">
+            <span className="ml-4 mt-3 text-sm text-gray-500 sm:text-base dark:text-gray-400">
+              <ul role="list" className="space-y-2">
                 {descriptionList.map((item, idx) => (
-                  <li key={idx} className="py-1">
-                    {item}
+                  <li key={idx} className="relative">
+                    <span className="static flex items-start">
+                      <CheckIcon className="hidden absolute -left-7 h-5 w-5 text-secondary-600 sm:inline-block dark:text-secondary-500" />
+                      <span>{item}</span>
+                    </span>
                   </li>
                 ))}
               </ul>
